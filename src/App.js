aa6a489aa7b601a30186c904344d9cfd13e055e0
S K Udara <mdsamarasinghe53@gmail.com>
2023-07-01 14:41:15 +0530

    first updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login'
+import ResetPassword from './pages/ResetPassword'
+import ForgotPassword from './pages/ForgotPassword'
+import MainLayout from './components/MainLayout'
+import Dashboard from './pages/Dashboard'
+import Enquiries from './pages/Enquiries';
+import BlogList from './pages/BlogList';
+import BlogCatList from './pages/BlogCatList';
+import Orders from './pages/Orders';
+import Customers from './pages/Customers';
+import ColorList from './pages/ColorList';
+import CategoryList from './pages/CategoryList';
+import BrandList from './pages/BrandList';
+import ProductList from './pages/ProductList';
+import AddBlog from './pages/AddBlog';
+import AddBlogCat from './pages/AddBlogCat';
+import AddColor from './pages/AddColor';
+import AddCat from './pages/AddCat';
+import AddBrand from './pages/AddBrand';
+import AddProduct from './pages/AddProduct';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/reset-password' element={<ResetPassword/>}/>
+        <Route path='/forgot-password' element={<ForgotPassword/>}/>
+        <Route path='/admin' element={<MainLayout/>}>
+        <Route index element={<Dashboard/>}/>
+        <Route path='enquiries' element={<Enquiries/>}/>
+        <Route path='list-blog' element={<BlogList/>}/>
+        <Route path='blog' element={<AddBlog/>}/>
+        <Route path='blog-category-list' element={<BlogCatList/>}/>
+        <Route path='blog-category' element={<AddBlogCat/>}/>
+        <Route path='orders' element={<Orders/>}/>
+        <Route path='customers' element={<Customers/>}/>
+        <Route path='list-color' element={<ColorList/>}/>
+        <Route path='color' element={<AddColor/>}/>
+        <Route path='list-category' element={<CategoryList/>}/>
+        <Route path='category' element={<AddCat/>}/>
+        <Route path='list-brand' element={<BrandList/>}/>
+        <Route path='brand' element={<AddBrand/>} />
+        <Route path='list-product' element={<ProductList/>}/>
+        <Route path='product' element={<AddProduct/>}/>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/ResetPassword.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import CustomInput from '../components/CustomInput'
+import { Link } from 'react-router-dom'
+
+const ResetPassword = () => {
+  return (
+    <div className='py-5' style={{background:"#ffd333",minHeight:"100vh"}}>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <div className='my-5 w-25 bg-white rounded-3 mx-auto p-4'>
+    <h3 className='text-center title'>Reset Password</h3>
+    <p className='text-center'>Please Enter your new password.</p>
+    <form action="">
+    <CustomInput type='password' label='Password' id="pass" />
+    <CustomInput type='password' label='Confirm Password' id="confirmpass" />
+    <Link 
+    className='border-0 px-3 py-2 text-white fw-bold w-100' 
+    style={{background:"#ffd333"}}
+    type='submit'
+    >Reset Password</Link>
+    </form>
+    </div>
+    </div>
+  )
+}
+
+export default ResetPassword
